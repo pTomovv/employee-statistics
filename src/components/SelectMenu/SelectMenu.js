09479306376ff2efb5c56dcf14d8fc6fc8776917
Petar Tomov <petarstoychevtomov@abv.bg>
2023-12-27 08:42:51 +0200

    solved the problem with case sensitivity

---
+++ src/components/SelectMenu/SelectMenu.js
@@ -0,0 +1,34 @@
+import styles from './SelectMenu.module.css';
+
+const SelectMenu = ({ options, onChange }) => {
+    function handleSelectChange(e) {
+        onChange(e.target.value);
+    }
+
+    return (
+        <select
+            name="employees"
+            id="employees"
+            className={styles.select}
+            onChange={handleSelectChange}
+            title="Select an employee"
+        >
+            <option value="all" className={styles.option}>
+                All Employees
+            </option>
+            {options.map((option) => {
+                return (
+                    <option
+                        key={option}
+                        value={option}
+                        className={styles.option}
+                    >
+                        {option}
+                    </option>
+                );
+            })}
+        </select>
+    );
+};
+
+export default SelectMenu;
